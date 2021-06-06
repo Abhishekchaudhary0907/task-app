7ec562984dd5c7611182ad00e58591f110253f00
sanatani <abhishek.abskch@gmail.com>
2021-06-06 09:01:47 +0530

    adding new routes

--- src/index.js
+++ src/index.js
@@ -12,9 +12,33 @@ app.post('/users',(req,res) =>{
     const user = new User(req.body);
     
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/users',(req,res) => {
+
+    const users = User.find({});
+    users.then((users) => {
+        res.status(201).send(users)
+    }).catch((e) => {
+        res.status(500).send("could not find")
+    })
+    
+})
+
+app.get('/users/:id', (req,res) => {
+    const id = req.params;
+    User.findById({_id: id.id}).then((user) => {
+        if(!user){
+            return res.status(400).send()
+        }
+
+        res.status(201).send(user);
+    }).catch(() => {
+        res.status(500).send();
     })
 })
 
@@ -22,11 +46,37 @@ app.post('/tasks', (req,res) => {
     const task = new Task(req.body);
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
 })
+
+app.get('/tasks', (req,res) => {
+
+    const tasks = Task.find({});
+    tasks.then((tasks) => {
+        res.status(201).send(tasks)
+    }).catch((e) => {
+        res.status(500).send("could not reach to server")
+    })
+})
+
+app.get('/tasks/:id',(req,res) => {
+    const id = req.params;
+
+    Task.findById({_id: id.id}).then((task) => {
+
+        if(!task){
+            return res.status(400).send();
+        }
+
+        res.status(201).send(task);
+    }).catch(() => {
+        res.status(500).send();
+    })
+})
+
 app.listen(portNumber,() => {
     console.log(`server is runnig on port 3000`)
 })
