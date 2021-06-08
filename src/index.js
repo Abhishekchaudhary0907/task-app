d6a401ef12741910c8c574135a38e3a1953cde98
sanatani <abhishek.abskch@gmail.com>
2021-06-08 11:21:37 +0530

    adding async await concept in router

--- src/index.js
+++ src/index.js
@@ -8,76 +8,83 @@ const portNumber = process.env.PORT || 3000;
 const app = express();
 app.use(express.json()); // for parsing the application json
 
-app.post('/users',(req,res) =>{
+app.post('/users',async(req,res) =>{
+
     const user = new User(req.body);
-    
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
+    try{
+       const result = await user.save();
+       res.status(201).send(result)
+    }catch(e){
+        res,status(400).send(e)
+    }
+
 })
 
-app.get('/users',(req,res) => {
+app.get('/users',async(req,res) => {
 
-    const users = User.find({});
-    users.then((users) => {
-        res.status(201).send(users)
-    }).catch((e) => {
-        res.status(500).send("could not find")
-    })
-    
+    try{
+        const users = await User.find({});
+        res.status(201).send(users);
+    }catch(e){
+        res.status(500).send(e);
+    }
+     
 })
 
-app.get('/users/:id', (req,res) => {
+app.get('/users/:id', async(req,res) => {
     const id = req.params;
-    User.findById({_id: id.id}).then((user) => {
-        if(!user){
-            return res.status(400).send()
-        }
-
-        res.status(201).send(user);
-    }).catch(() => {
-        res.status(500).send();
-    })
-})
-
-app.patch('/users/:id', (req, res) => {
-    const _id = req.params.id;
-    User.findByIdAndUpdate(_id,req.body).then((user) => {
+    try{
+        const user = await User.findById({_id: id.id});
         if(!user){
             return res.status(400).send();
         }
 
         res.status(201).send(user);
-    }).catch((e) => {
-        res.status(500).send("error from the server");
-    })
+    }catch(e){
+        res.status(500).send();
+    }
+    
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    try{
+        const user = await User.findByIdAndUpdate(_id,req.body)
+        if(!user){
+           return res.status(400).send()
+        }
 
+        res.status(401).send(user);
+    }catch(e){
+        res.status(500).send();
+    }
+   
+})
 
-app.post('/tasks', (req,res) => {
+app.post('/tasks', async(req,res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
+    try{
+        await task.save()
+        res.status(201).send(task);
+    }catch(e){
+        res.status(400).sen(e);
+    }
+    
 })
 
-app.get('/tasks', (req,res) => {
+app.get('/tasks',async(req,res) => { 
 
-    const tasks = Task.find({});
-    tasks.then((tasks) => {
-        res.status(201).send(tasks)
-    }).catch((e) => {
-        res.status(500).send("could not reach to server")
-    })
+    try{
+        const tasks = await Task.find({});
+        res.status(201).send(tasks);
+    }catch(e){
+        res.status(500).send("could not connected");
+    }
+    
 })
 
-app.get('/tasks/:id',(req,res) => {
+app.get('/tasks/:id',(req,res) => { // async me nhi implement kiya baad me krunga
     const id = req.params;
 
     Task.findById({_id: id.id}).then((task) => {
@@ -92,18 +99,20 @@ app.get('/tasks/:id',(req,res) => {
     })
 })
 
-app.patch('/tasks/:id',(req,res) => {
+app.patch('/tasks/:id',async(req,res) => {
   const _id = req.params.id;
   
-  Task.findByIdAndUpdate(_id, req.body).then((task) => {
-      if(!task){
-         return  res.status(400).send()
-      }
-      res.status(201).send(task);
+  try{
+    const task = await Task.findByIdAndUpdate(_id, req.body,{new: true})
 
-  }).catch((e) => {
-      res.status(500).send()
-  })
+    if(!task){
+        res.status(400).send();
+    }
+    res.status(201).send(task)
+  }catch(e){
+        res.status(500).send(e)
+  }
+  
 })
 
 app.listen(portNumber,() => {
---
+++ src/promise-chaining.js
@@ -0,0 +1,26 @@
+require('./db/mongoose');
+const Task = require('./models/task');
+const User = require('./models/user');
+
+
+// Task.findByIdAndRemove("60bed9a72591ec29e4ae7c91").then(() => {
+//     console.log("removed successfully");
+//     return Task.countDocuments({completed: false})
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteAndCount = async(id,complete) => {
+
+    await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments(complete);
+    return count
+}
+
+deleteAndCount("60bedb9f1637222c3cc6313a",{completed:false}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
