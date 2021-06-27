39f969f4916fcd184b68f18767cc2efba3eee0a6
sanatani <abhishek.abskch@gmail.com>
2021-06-27 09:08:33 +0530

    final task-app

--- src/index.js
+++ src/index.js
@@ -4,22 +4,9 @@ const taskRouter = require('./routers/task');
 
 require("./db/mongoose");  // run mongodb server
 
-const portNumber = process.env.PORT || 3000;
+const portNumber = process.env.PORT;
 const app = express();
-app.use(express.json()); // for parsing the application json
-
-// middleware method which apply all the router because it is not mounted
-// app.use(function(req, res, next){
-//     console.log(req.method);
-//     next();
-// }, function(req, res, next){
-//     console.log(req.originalUrl);
-//     next();
-// });
-
-// app.use(function(req,res, next){
-//     res.status(503).send("site under mentainence");
-// })
+app.use(express.json()); 
 
 app.use(userRouter);
 app.use(taskRouter);
@@ -28,18 +15,3 @@ app.listen(portNumber,() => {
     console.log(`server is runnig on port ${portNumber}`)
 })
 
-const Task = require('./models/task');
-const User = require('./models/user');
-
-
-const main = async() => {
-    // const task = await Task.findById('60d0146c69ffd4374c9a36c9');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    // const user = await User.findById('60d00be1cbcecb2af8c5695c');
-    // await user.populate('tasks').execPopulate();
-    // console.log(user.tasks);
-}
-
-main();
